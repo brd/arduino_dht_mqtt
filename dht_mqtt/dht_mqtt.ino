b4487d934f2a7a148dfece03a17c0200df942517
Brad Davis <brd@FreeBSD.org>
2020-03-25 14:44:15 -0600

    Add loop for connecting to MQTT and set the will packet

--- dht_mqtt/dht_mqtt.ino
+++ dht_mqtt/dht_mqtt.ino
@@ -38,13 +38,16 @@ byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0x30, 0xF5 };
 #define MQTT_SERVERPORT  1883
 #define MQTT_USERNAME    ""
 #define MQTT_KEY         ""
-
+#define MQTT_RETRIES     3
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
 // that you want to connect to (port 23 is default for telnet;
 // if you're using Processing's ChatServer, use  port 10002):
 EthernetClient client;
+
+// Have we sent the status packet (setup for the will packet)
+uint32_t status_sent = 0;
 
 // Initialize Adafruit_MQTT
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
@@ -106,21 +109,31 @@ void loop() {
 
 
 // Function to connect and reconnect as necessary to the MQTT server.
-void MQTT_connect() {
+int MQTT_connect() {
   int8_t ret;
+  int8_t loop = 0;
 
   // Stop if already connected.
   if (mqtt.connected()) {
-    return;
+    return 1;
   }
 
   Serial.print("Connecting to MQTT... ");
 
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+  // Set will
+  // will(const char *topic, const char *payload, uint8_t qos, uint8_t retain)
+  mqtt.will("house/status/" LOCATION, "false", 1, 1);
+
+  while ((ret = mqtt.connect()) != 0 && loop <= MQTT_RETRIES) { // connect will return 0 for connected
        Serial.println(mqtt.connectErrorString(ret));
        Serial.println("Retrying MQTT connection in 5 seconds...");
        mqtt.disconnect();
+       loop++;
        delay(5000);  // wait 5 seconds
   }
-  Serial.println("MQTT Connected!");
+  if(ret == 0) {
+    Serial.println("MQTT Connected!");
+  }
+
+  return 0;
 }
